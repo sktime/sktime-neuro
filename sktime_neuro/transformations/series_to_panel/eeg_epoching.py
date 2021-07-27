7b1cdffb626da47ae3daeac05de492d0111d2585
Svea Marie Meyer <svea.meyer@tum.de>
2021-07-27 13:44:33 +0200

    allow epoching to take list of labels and return data and labels vector

--- sktime_neuro/transformations/series_to_panel/eeg_epoching.py
+++ sktime_neuro/transformations/series_to_panel/eeg_epoching.py
@@ -1,62 +1,59 @@
 # -*- coding: utf-8 -*-
 __author__ = ["Svea Marie Meyer"]
-__all__ = ["Epoch"]
 
-
-from sktime_neuro.transformations.base import _SeriesToPanelTransformer
 import numpy as np
 from sktime.utils.validation.series import check_series
 
 
-class Epoch(_SeriesToPanelTransformer):
+def epoch(Z, annotation, labels, interval, sfreq) -> (np.array, np.array):
     """
-    Epoch continuous EEG data into trials of label.
+    Parameters
+    _________
+    annotation : pd.DataFrame,
+        one row per event with columns "onset", "duration"
+        and "descritption"
+        can be create from mne raw object
+        with `sktime_neuro.utils.mne_processing.create_annotation`
+    labels : list of string
+        labels of events to create trials from
+    interval : tuple of float or int
+        time interval around event to select
+    fs : int or float
+        sampling frequency of the recorded data in Hz
+
+    Returns
+    ________
+    Xt : np.array
+        panel data (shape: trials, channels, timepoints)
+    y : np.array
+        labels vector
     """
 
-    def __init__(self, annotation, label, interval, sfreq):
-        self.annotation = annotation
-        self.label = label
-        self.interval = interval
-        self.sfreq = sfreq
-        super(Epoch).__init__()
+    Z = check_series(Z)
 
-    def transform(self, Z, y=None) -> np.array:
-        """
-        Parameters
-        _________
-        annotation : pd.DataFrame,
-            one row per event with columns "onset", "duration"
-            and "descritption"
-            can be create from mne raw object
-            with `sktime_neuro.utils.mne_processing.create_annotation`
-        label : string
-            label of event to create trials from
-        interval : tuple of float or int
-            time interval around event to select
-        fs : int or float
-            sampling frequency of the recorded data in Hz
+    # create shape of final data
+    n_channels = Z.shape[0]
+    n_timepoints = int(interval[1] * sfreq) - int(interval[0] * sfreq)
+    n_trials = len(annotation.loc[lambda df: df["description"].isin(labels)]["onset"])
+    X = np.zeros((n_trials, n_channels, n_timepoints))
+    y = []
 
-        Returns
-        ________
-        Xt : np.array
-             panel data with trials of event "label"
-        """
-        Z = check_series(Z)
-        all_onsets = self.annotation.loc[lambda df: df["description"] == self.label][
-            "onset"
-        ]
-        n_trials = len(all_onsets)
-        n_channels = Z.shape[0]
-        n_timepoints = int(self.interval[1] * self.sfreq) - int(
-            self.interval[0] * self.sfreq
-        )
-        Xt = np.zeros((n_trials, n_channels, n_timepoints))
-        for idx, onset in enumerate(all_onsets):
-            offset = int(self.sfreq * onset)
-            Xt[idx] = Z[
+    # iterate over specified labels and get corresponding trials
+    idx = 0
+    for label in labels:
+
+        # get all onsets that correspond to the label we are currently looking at
+        onsets_of_label = annotation.loc[lambda df: df["description"] == label]["onset"]
+
+        # iterate over onsets and add them to the datacontainer (X) and labels (y)
+        for onset in onsets_of_label:
+            offset = int(sfreq * onset)
+            X[idx] = Z[
                 :,
-                (int(self.interval[0] * self.sfreq) + offset) : (
-                    int(self.interval[1] * self.sfreq) + offset
+                (int(interval[0] * sfreq) + offset) : (
+                    int(interval[1] * sfreq) + offset
                 ),
             ]
-        return Xt
+            y.append(label)
+            idx += 1
+    return X, np.asarray(y)
