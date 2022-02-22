4d5f738431b26106dead5e6df89cb17363654ef7
Emilia <tjd17qcu@uea.ac.uk>
2022-02-22 20:35:42 +0000

    Chunking implemented

--- sktime_neuro/datasets/conversion/european_mne.py
+++ sktime_neuro/datasets/conversion/european_mne.py
@@ -5,6 +5,7 @@ from typing import List, Dict
 import sktime_neuro.utils.mne_processing
 from sktime.datasets._data_io import write_dataframe_to_tsfile
 from sktime.datasets._single_problem_loaders import load_UCR_UEA_dataset
+import time
 
 
 
@@ -48,7 +49,6 @@ if __name__ == "__main__":
     annotation = mne.read_annotations(af[1])
     raw.set_annotations(annotation)
 
-    a = mne.events_from_annotations(raw) #Get a list of all the events and their corresponding annotation, time is in samples
     eventList = mne.events_from_annotations(raw)[0] #Times (in samples) and the class they belong to
     labels = mne.events_from_annotations(raw)[1] # Names for the class labels
     #need to find sampel rate, and data for each channel, and write that into a ts file with the annotations (class label)
@@ -65,4 +65,35 @@ if __name__ == "__main__":
     print("Last sample = " + str(raw.last_samp))
     print("Samp freq = " + str(raw.info['sfreq']))
     print(len(channelList[0]['Data'][0][0])) #Gets the total samples, Have to dive a bit to get some of this info, blame nparrays
+    print(eventList[1][0]) # Get start of 2nd event
+    print(len(eventList)) # Get num events
+    events_from_annot, events = mne.events_from_annotations(raw)
+
+
+    epochs_train = mne.Epochs(raw=raw, events=events_from_annot,event_id=None, baseline=None) # Not useful
+    print("d")
+
+    before = time.time()
+    classData = []
+    counter = 0
+    #Iteerate though all channels and store (seperate each channel) until we reach a new event, in which create new array
+    while counter < len(eventList): #While we still got events to process
+        classData.append([])
+
+        #list subthingy
+        min = eventList[counter][0]
+        if counter + 1 >= len(eventList):
+            max = len(channelList[0]['Data'][0][0]) ## Total samples
+        else:
+            max = eventList[counter+1][0]
+        tempArr = [eventList[counter][2]] # Set what class it belongs to
+        for i in range(len(channelList)): #Iterate through the channels
+            tempArr.append(channelList[i]['Data'][0][0][min:max])
+        classData[-1].append(tempArr) #Append to current working list
+        counter += 1
+
+        print(counter)
+    total = time.time() - before
+    print(total)
+    #chunk up the data for each channel into each class, and then store it in a list
     #write_dataframe_to_tsfile()
